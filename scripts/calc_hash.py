94cdb33970f3433cb46e3fcae6d4242407c52d4a
Nikolas Dahn <nikolas.dahn@dfki.de>
2026-01-30 09:20:24 +0100

    calc_hash script now opens the dialog if no args are provided

--- scripts/calc_hash.py
+++ scripts/calc_hash.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import sys
+import tkinter as tk
 
 from pybnk.util import calc_hash
+from pybnk.gui.calc_hash_dialog import CalcHashDialog
+from pybnk.gui.localization import English
 
 
 if __name__ == "__main__":
-    
-    for arg in sys.argv[1:]:
-        h = calc_hash(arg)
-        print(f"{arg}:\t{h}")
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            h = calc_hash(arg)
+            print(f"{arg}:\t{h}")
+    else:
+        app = tk.Tk()
+        app.withdraw()
+        dlg = CalcHashDialog(None, English())
+        app.mainloop()
